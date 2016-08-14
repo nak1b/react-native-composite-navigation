cc4da7da8653f0dc4e1fa9572733dce7a5e34808
Nakib <nbmomin14@gmail.com>
2016-08-14 15:58:20 -0700

    Added App with provider

---
+++ app/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+import reducers from './reducers/index'
+import NavigationContainer from './containers/NavigationContainer'
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+const store = createStoreWithMiddleware(reducers)
+
+
+class App extends Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<NavigationContainer />
+			</Provider>
+		)
+	}
+} 
+
+
+export default App
